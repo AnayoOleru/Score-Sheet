9f9041dab8c9d36dd4cfd5b88a4fec7f5c86494a
Anayo Oleru <anayo_oleru@outlook.com>
2019-07-20 06:53:42 +0100

    ft-add-functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+//select every single one fo the check boxes
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
